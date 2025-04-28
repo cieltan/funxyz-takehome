2ca4805a64ae63f9968e6338abd05e3a0e0d0974
Billy Tan <btan7645@gmail.com>
2025-04-28 18:44:19 -0400

    Refactor Home component by removing search parameter handling and state management; move logic to SwapContainer for improved separation of concerns and maintainability.

--- src/components/swap/SwapContainer.tsx
+++ src/components/swap/SwapContainer.tsx
@@ -1,11 +1,29 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SwapDefaultView from "./SwapDefaultView";
 import SwapSelectTokenView from "./SwapSelectTokenView";
 import { SwapContainerView, SwapSide } from "@/utils/const"
+import { setTokenSource, setTokenTarget } from "@/store/features/swapSlice";
+import { setSourcePriceData } from "@/store/features/swapSlice";
+import { setTargetPriceData } from "@/store/features/swapSlice";
+import { useFun } from "../hooks/useFun";
+import { Token } from "@/types/token";
+import { useSearchParams } from "next/navigation";
+import { useAppDispatch } from "@/store/hooks";
 
 const SwapContainer = () => {
+    const searchParams = useSearchParams();
+    const dispatch = useAppDispatch();
+
+    const sourceSymbol = searchParams.get('source') as Token | null;
+    const targetSymbol = searchParams.get('target') as Token | null;
+    const apiKey = process.env.NEXT_PUBLIC_FUNXYZ_API_KEY || '';
+
+    const sourceData = useFun({ symbol: sourceSymbol, apiKey });
+    const targetData = useFun({ symbol: targetSymbol, apiKey });
+
+
     const [view, setView] = useState<SwapContainerView>(SwapContainerView.DEFAULT);
     const [selectedSwapSide, setSelectedSwapSide] = useState<SwapSide | null>(null);
     const renderView = () => {
@@ -27,6 +45,20 @@ const SwapContainer = () => {
                 );
         }
     }
+
+    useEffect(() => {
+        if (sourceData.tokenInfo && sourceData.price) {
+            dispatch(setTokenSource(sourceData.tokenInfo));
+            dispatch(setSourcePriceData(sourceData.price));
+        }
+    }, [sourceData, dispatch]);
+
+    useEffect(() => {
+        if (targetData.tokenInfo && targetData.price) {
+            dispatch(setTokenTarget(targetData.tokenInfo));
+            dispatch(setTargetPriceData(targetData.price));
+        }
+    }, [targetData, dispatch]);
 
     return (
         <div className="flex flex-col relative bg-background shadow-[0.8rem_0.8rem_1.4rem_var(--foreground),-0.2rem_-0.2rem_1.8rem_var(--white)] p-3 sm:p-4 md:p-6 rounded-lg gap-3 sm:gap-4 w-full">
